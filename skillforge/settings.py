3d62fc0cc7c20650ce6aa444fde654000e8c2b18
Sujal Chavan <sujalchavan035@gmail.com>
2025-12-07 16:20:55 +0530

    Updated static settings for Render production

--- skillforge/settings.py
+++ skillforge/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-884kk7yr9y*ue(6*y-u*h25zvxze&8^ib=&7)8ul#snsyv_gtn'
-
+import os
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-884kk7yr9y*ue(6*y-u*h25zvxze&8^ib=&7)8ul#snsyv_gtn")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -118,9 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
